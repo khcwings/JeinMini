aac7988d0b7fd4b88271fc98b0604e1fd22c84f2
JEINSOFT <JEINSOFT@JEINSOFT-PC>
2018-12-11 10:13:05 +0900

    2018-12-11 김희철 Thymeleaf Version 변경 - 사유 : 하위 버전시 Layout 지원에서 오류 발생

--- JeinMini/src/main/java/com/jein/mini/web/config/ServletConfiguration.java
+++ JeinMini/src/main/java/com/jein/mini/web/config/ServletConfiguration.java
@@ -1,20 +1,10 @@
 package com.jein.mini.web.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Description;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.resource.VersionResourceResolver;
-import org.thymeleaf.spring4.SpringTemplateEngine;
-import org.thymeleaf.spring4.view.ThymeleafViewResolver;
-import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
-import lombok.extern.java.Log;
-import nz.net.ultraq.thymeleaf.LayoutDialect;
-
-@Log
 @Configuration
 public class ServletConfiguration extends WebMvcConfigurerAdapter {
 	@Override
@@ -40,46 +30,4 @@ public class ServletConfiguration extends WebMvcConfigurerAdapter {
 		resourceChain(false).
 		addResolver(new VersionResourceResolver().addContentVersionStrategy("/**"));
 	}
-	
-	@Bean
-    public LayoutDialect layoutDialect() {
-        return new LayoutDialect();
-    }
-
-	/*@Bean
-	@Description("Thymeleaf template resolver serving HTML 5")
-	public ClassLoaderTemplateResolver templateResolver() {
-		log.info("##### Thymeleaf template resolver serving HTML 5");
-		ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
-
-		templateResolver.setPrefix("templates/");
-		templateResolver.setCacheable(false);
-		templateResolver.setSuffix(".html");        
-		templateResolver.setTemplateMode("HTML5");
-		templateResolver.setCharacterEncoding("UTF-8");
-
-		return templateResolver;
-	}
-
-	@Bean
-	@Description("Thymeleaf template engine with Spring integration")
-	public SpringTemplateEngine templateEngine() {
-		log.info("##### Thymeleaf template engine with Spring integration");
-		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-		templateEngine.setTemplateResolver(templateResolver());
-
-		return templateEngine;
-	}
-
-	@Bean
-	@Description("Thymeleaf view resolver")
-	public ViewResolver viewResolver() {
-		log.info("##### Thymeleaf view resolver");
-		ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
-
-		viewResolver.setTemplateEngine(templateEngine());
-		viewResolver.setCharacterEncoding("UTF-8");
-
-		return viewResolver;
-	}    */
 }
